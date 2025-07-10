e7bbfb3155b396832aaf8575c1b1887c26f847c4
carlottaarmenise-job <101394461+carlottaarmenise-job@users.noreply.github.com>
2025-07-10 16:41:42 +0200

    add image and change ui

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
+  Image,
 } from 'react-native';
 import { theme } from '../assets/styles/styles';
 
@@ -45,58 +46,64 @@ const SignUpScreen = () => {
 
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.title}>CREA IL TUO ACCOUNT</Text>
+      <View style={styles.header}>
+        <Image source={require('../assets/logo.png')} style={{ width: 150, height: 130 }} />
+      </View>
+      <View style={{ backgroundColor: 'white', paddingTop: 20, paddingBottom: 20, padding: 16 }}>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome"
-        placeholderTextColor="#ccc"
-        value={form.nome}
-        onChangeText={(text) => handleChange('nome', text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Cognome"
-        placeholderTextColor="#ccc"
-        value={form.cognome}
-        onChangeText={(text) => handleChange('cognome', text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        placeholderTextColor="#ccc"
-        value={form.email}
-        onChangeText={(text) => handleChange('email', text)}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Telefono"
-        placeholderTextColor="#ccc"
-        value={form.telefono}
-        onChangeText={(text) => handleChange('telefono', text)}
-        keyboardType="phone-pad"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        placeholderTextColor="#ccc"
-        secureTextEntry
-        value={form.password}
-        onChangeText={(text) => handleChange('password', text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Conferma Password"
-        placeholderTextColor="#ccc"
-        secureTextEntry
-        value={form.confermaPassword}
-        onChangeText={(text) => handleChange('confermaPassword', text)}
-      />
+        <Text style={styles.title}>CREA IL TUO ACCOUNT</Text>
 
-      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-        <Text style={styles.buttonText}>ISCRIVITI</Text>
-      </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome"
+          placeholderTextColor="#ccc"
+          value={form.nome}
+          onChangeText={(text) => handleChange('nome', text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Cognome"
+          placeholderTextColor="#ccc"
+          value={form.cognome}
+          onChangeText={(text) => handleChange('cognome', text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#ccc"
+          value={form.email}
+          onChangeText={(text) => handleChange('email', text)}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Telefono"
+          placeholderTextColor="#ccc"
+          value={form.telefono}
+          onChangeText={(text) => handleChange('telefono', text)}
+          keyboardType="phone-pad"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#ccc"
+          secureTextEntry
+          value={form.password}
+          onChangeText={(text) => handleChange('password', text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Conferma Password"
+          placeholderTextColor="#ccc"
+          secureTextEntry
+          value={form.confermaPassword}
+          onChangeText={(text) => handleChange('confermaPassword', text)}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+          <Text style={styles.buttonText}>ISCRIVITI</Text>
+        </TouchableOpacity>
+      </View>
 
     </ScrollView>
   );
@@ -108,30 +115,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.primary,
-    padding: 20,
+  },
+  header: {
+    paddingTop: 20,
+    paddingBottom: 10,
+    paddingHorizontal: 15,
+    backgroundColor: theme.primary,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: theme.secondary,
   },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
-    color: 'white',
+    color: theme.secondary,
     marginBottom: 20,
   },
+
   input: {
     backgroundColor: 'white',
     borderRadius: 8,
-    padding: 12,
-    fontSize: 16,
     marginBottom: 15,
+    padding: 10,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
   },
   button: {
-    backgroundColor: 'white',
-    padding: 15,
+    backgroundColor: theme.primary,
+    padding: 16,
     borderRadius: 8,
+    marginTop: 30,
     alignItems: 'center',
-    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
   buttonText: {
-    color: theme.primary,
+    color: 'white',
     fontWeight: 'bold',
+    fontSize: 14,
   },
+
 });
